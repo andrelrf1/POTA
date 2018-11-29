1ea415b9a2ec79ff49cea2c645a539217a0d9876
andrelrf1 <andre_al1@live.com>
2018-11-28 22:55:46 -0300

    Atualização 3: - Finalização do pacote dos Sorts - Criação do módulo controlador - Atualização do módulo main - Programa funcional

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Main(object):
     def __init__(self):
         while True:
             print("-" * 50)
-            print("Comparação de desempenho de sorts".upper().center(50))
+            print("Comparação de desempenho entre sorts".upper().center(50))
             print("-"*50)
             print("\n1 - Tamanho 5\n2 - Tamanho 10\n3 - Tamanho 50\n4 - Tamanho 100\n5 - Tamanho 1000 (Lento)\n"
                   "6 - Tamanho 10000 (Inviável)\n0 - Cancelar\n")
